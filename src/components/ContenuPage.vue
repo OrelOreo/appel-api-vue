9d2baeeb15b998ae79d9a4e53fab6bc96259c67b
OrelOreo <oreloreopro@gmail.com>
2022-11-15 11:39:48 +0100

    first commit

---
+++ src/components/ContenuPage.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <div class="container">
+        <h1>Appel à une API</h1>
+        <img v-bind:src="urlImg">
+    </div>
+
+</template>
+
+
+
+<script>
+
+import axios from 'axios'
+
+
+export default {
+    name:'ContenuPage',
+    data(){
+        return {
+            urlImg: null
+        }
+    },
+    mounted(){
+        axios
+        .get('https://api.thecatapi.com/v1/images/search')
+        .then(reponse => {
+            this.urlImg = reponse.data[0].url
+        })
+    }
+}
+
+
+
+</script>
+
+
+<style>
+
+
+</style>
